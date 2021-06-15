3e05f04245c072e59c96ea9fab0ab74997d35e2c
huangwei <hw758214335@gmail.com>
2021-06-15 21:52:08 +0800

    Tabbar

---
+++ hospital/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import Taro from '@tarojs/taro'
+import {Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+
+export default class Index extends Component {
+    
+    render() {
+      return (
+        <View>test</View>
+      )
+    }
+}
