a7b35b26fb65443690d320b5ea4e7d2af3ed4d53
huangwei <hw758214335@gmail.com>
2021-06-29 13:17:35 +0800

    Test git

--- hospital/src/pages/home/index.tsx
+++ hospital/src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import Taro from '@tarojs/taro'
 import {Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
-
+// test
 export default class Index extends Component {
     
     render() {
