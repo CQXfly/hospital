429c426c265a2f7dcac164461aaaea53b9d14e51
huangwei <hw758214335@gmail.com>
2021-08-30 09:44:57 +0800

    既往病史cell显示image

--- src/components/MyAccordion/index.tsx
+++ src/components/MyAccordion/index.tsx
@@ -1,9 +1,11 @@
 // import { View } from "@tarojs/components";
+import { View, Image, Text } from "@tarojs/components";
 import { Component } from "@tarojs/taro";
-import { AtAccordion, AtList, AtListItem } from "taro-ui";
+import { AtAccordion, AtList, AtListItem, AtIcon} from "taro-ui";
 
 // 
 import { Disease } from "../../common/DiseaseInterfaces";
+import './index.scss'
 
 
 // interface MyProps {
@@ -37,8 +39,18 @@ export default class MyAccordion extends Component<Disease> {
             title={this.props.name}
             note={this.props.date} >
               <AtList hasBorder={false}>
-                <AtListItem title={this.props.dep} extraText={this.props.hosptital} note={this.props.type}/>
-                <AtListItem note={this.props.info}/>
+                {/* <AtListItem title={this.props.dep} extraText={this.props.hosptital} note={this.props.type}/>
+                <AtListItem note={this.props.info}/> */}
+                <View className='subInfoBack'>
+                    <Text className='subInfo'>{this.props.hosptital}</Text>
+                    <Text className='subInfo'>{this.props.type}</Text>
+                </View>
+                <View className='description'>{this.props.info}</View>
+                <View className='photoArea'>
+                    <Image className='icon' src='https://staticweb.keepcdn.com/staticShow/images/appintro/app_pic_t2_class16-914ffa6fb1.png' mode={"aspectFill"}></Image>
+                    <Image className='icon' src='https://staticweb.keepcdn.com/staticShow/images/appintro/app_pic_t2_class16-914ffa6fb1.png' mode={"aspectFill"}></Image>
+                    <Image className='icon' src='https://staticweb.keepcdn.com/staticShow/images/appintro/app_pic_t2_class16-914ffa6fb1.png' mode={"aspectFill"}></Image>
+                </View>
               </AtList>
             </AtAccordion>
         )
